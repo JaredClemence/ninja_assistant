f01f6c00e2c931591e1c771051ed465a4a87fb38
Jared R Clemence <jaredclemence@gmail.com>
2019-12-12 11:35:55 -0800

    Updated header to include the application name.

--- resources/views/nav/main-menu.blade.php
+++ resources/views/nav/main-menu.blade.php
@@ -1,5 +1,7 @@
-<nav class="navbar navbar-expand-md navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Expand at md</a>
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">  
+  <a class="navbar-brand" href="{{ url('/') }}">
+    {{ config('app.name', 'Laravel') }}
+  </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
