7cd0b0a522c17b910e4ffaf0275a979eef6c6647
Jared R Clemence <jaredclemence@gmail.com>
2020-01-06 08:29:40 -0800

    Changed copy on success message to inform users to wait for an email.

--- resources/views/contacts/upload/intermediate_success.blade.php
+++ resources/views/contacts/upload/intermediate_success.blade.php
@@ -9,12 +9,14 @@
             <div class="row"><div class="col-12">
             Contact records approved.
             </div></div>
-        </div>
-        <p>
+        </div><p>
             You have approved the records. They will now be combined with your 
             existing list of contacts. Please return in several minutes after the 
             system has had time to process your request.
         </p>
+        <p><strong>If this is your first contact upload...</strong> close your browser and return <strong>after</strong>
+            you get the email noting that processing is complete.</p>
+        
     </div>
 </div>
 @endsection
